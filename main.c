3f2b166376bd5a74eecbeabd7be412ee99d69f26
Abir-Hasan-Khan <ashrafulhasanabir@gmail.com>
2026-06-04 08:01:51 +0600

    Create main.c

---
+++ main.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+char operator;
+double num1,num2,result;
+printf("Enter an operator (+,-,*,/):");
+scanf("%c",&operator);
+printf("Enter two numbers:");
+scanf("%lf %lf",&num1,&num2);
+switch(operator) {
+case '+' :
+result= num1+ num2;
+printf("result=%.2lf",result);
+break;
+case '-' :
+result=num1-num2;
+printf("result=%.2lf",result);
+break;
+case '*':
+result=num1*num2;
+printf("result=%.2lf",result);
+break;
+case '/':
+if(num2!=0) {
+result= num1/ num2;
+printf("result=%.2lf",result);
+}
+else {
+printf(" Error! Division by Zero is not allowed",result);
+}
+default:
+printf("Error! Invalid operator.\n");
+}
+return 0;
+}
